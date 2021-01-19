4de1c2e4bb32b24af5043d4e75e31929ba6b2203
Iretibe <yessouf2009@yahoo.fr>
2021-01-19 07:40:40 +0000

    first commit

---
+++ LaundrySystem.UI/Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LaundrySystem.UI.Controllers
+{
+    public class AccountController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public IActionResult Login()
+        //{
+        //    return View();
+        //}
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public IActionResult Register()
+        //{
+        //    return View();
+        //}
+    }
+}
